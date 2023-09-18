19f0e4ebc359e631aaaef6349fcb268991732898
Ak-Yadav3122 <144606935+Ak-Yadav3122@users.noreply.github.com>
2023-09-18 10:52:06 +0000

    opeartions

---
+++ JavaScript/04Operation.js
@@ -0,0 +1,19 @@
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// Some Arthimatic Operation are;
+
+//  console.log(3+5);
+//  console.log(3-5);
+//  console.log(3*5);
+//  console.log(3**5);
+//  console.log(3/5);
+//  console.log(3%5);
+
+let gameCounter = 99;
+
+gameCounter++;  //postfix operation
+++gameCounter;  //prefix operation
+
+console.log(gameCounter);
