ec45bfbec92a7d0538eb0020deb7f0bcd03e58a0
Ak-Yadav3122 <144606935+Ak-Yadav3122@users.noreply.github.com>
2023-09-18 16:43:26 +0530

    Update 01Variable.js

--- JavaScript/01Variable.js
+++ JavaScript/01Variable.js
@@ -5,7 +5,7 @@ let userCountry;
  userName = "Golu Yadav";
  userGmail = "golu1334@gmail.com"; 
  
-// Its gives error beacuse we use const keyword to make a variable and if we usee const keyword thwn we can not change the value.
+// Its gives error beacuse we use "const" keyword to make a variable and if we use "const" keyword then we can not change the value.
 //accountId = 435454; 
 
 console.table([accountId,userName,userGmail,userCountry]);
