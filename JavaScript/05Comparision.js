43ad701b50f98a2af74e5e4afe31314ec94d634b
Ak-Yadav3122 <144606935+Ak-Yadav3122@users.noreply.github.com>
2023-09-18 11:10:54 +0000

    comparision

---
+++ JavaScript/05Comparision.js
@@ -0,0 +1,18 @@
+// same data type
+
+console.log(2>1);
+console.log(2>=1);
+console.log(2==1);
+console.log(2<=1);
+
+//different datatype
+
+console.log("2">1);
+console.log(2 > "1");
+console.log("2"> true);
+
+// == , it is use for conversion of number and string
+console.log(2=="2");  //output = true because (==)change the string into number
+
+//===, it is use for strickely check the datatype then perform comparsion i.e;
+ console.log(2==="2");  //output = false because both are different datatype
