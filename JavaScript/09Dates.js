8079d008071f463403979d575fecc97d506623fd
Ak-Yadav3122 <144606935+Ak-Yadav3122@users.noreply.github.com>
2023-09-19 07:52:21 +0000

    Dates in Javascript

---
+++ JavaScript/09Dates.js
@@ -0,0 +1,16 @@
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+let myCreatedDate =new Date(2023, 1, 23) //formate of date = (year,month,date) , in JS value are start from 0
+let CreatedDate =new Date("01-14-2023")  // another way to write the date
+//console.log(CreatedDate.toDateString());
+
+let ourDate = new Date()
+console.log(ourDate.getTime());
+
