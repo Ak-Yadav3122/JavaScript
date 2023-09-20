8e81980d0a2f6dcef657b57bb4f3f39114683da5
Ak-Yadav3122 <144606935+Ak-Yadav3122@users.noreply.github.com>
2023-09-20 07:14:55 +0530

    Update 02DataType.js

--- 01.JavaScript/02DataType.js
+++ 01.JavaScript/02DataType.js
@@ -1,18 +1,18 @@
-"use strict"; //treat all js code as newer version
+"use strict"; //treat all js code as a newer version
 
 // alert(3 + 3); // Give arror because we are using node.js not browser
 
 let name = "Aichchhik"
 let age = 22;
  
-/* Different type of DataTypes;
+/* Different types of DataTypes;
 
   1. number
   2. String => written in double(" ") or single (' ') 
   3. Boolean => true or false
-  4. null => standalone value(It is representation of enpty value and it is a object)
-  5.undefined => we only make the variable not assign the value eg; let country;
-  6.symbol => use for unique value
+  4. null => standalone value(It is representation of empty value and it is a type of  object)
+  5. undefined => we only make the variable not assign the value eg; let country;
+  6. symbol => use for unique value
 
 */
 
