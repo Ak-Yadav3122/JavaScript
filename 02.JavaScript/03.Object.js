2002b3a6b6343389802dfd0835249fc02ce3582d
Ak-Yadav3122 <144606935+Ak-Yadav3122@users.noreply.github.com>
2023-09-20 08:29:16 +0000

    about object and JSON formate

---
+++ 02.JavaScript/03.Object.js
@@ -0,0 +1,72 @@
+ /*
+   Object are the collection of keys and values within the {} brackets.
+
+   There are two types of initialization or making of object i.e; object literals and object Constructor
+
+   Jab object constructor se banega tab singleton banega and jab object literals
+   ke help se banega tab singleton nahi banega.
+   
+   Singleton means ye apne tarah ka ek he object hai.
+   
+   */
+
+// this is object constructor
+// Object.create()
+
+//this is object literals
+const mySymb = Symbol("key1")  // Declarationn of symbol by using Symbol("jiska symbol banana hai use likhenge")
+
+const user = {
+    name: "Aichchhik", // we can also write name = "name".Both are same
+    "full name": "Aichchhik",
+    [mySymb]: "mykey1",
+    age: 18,
+    location: "Jaipur",
+    email: "aichchcik@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+// There are two ways to access the the key of obejct i.e;
+// (.write which key we want)method and ["write which key we want"] method.
+
+// console.log(user.email)
+// console.log(user["email"])
+// console.log(user["full name"])
+// console.log(user[mySymb])
+
+user.email = "golu12@gmail.com"
+
+// Object.freeze(User) // Object.freeze(which we want to freeze) , this method is used to freeze the object.After performing this operation any one can not change the keys value.
+
+user.email = "Aichchhikyadav@gmail.com"
+// console.log(User);
+
+user.greeting = function(){
+    console.log("Hello JS user");
+}
+user.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(user.greeting());
+console.log(user.greetingTwo());
+
+
+/* IMPORTANT TO Know...
+
+ Q: How to use symbol as a key in object.
+ Ans: we use [] and write our variable which we want to use.
+
+ Ex:- const mySymb = Symbol("key1")  
+      const user{
+        [mySymb]: "mykey1"
+      }
+      console.log(user[mySymb])
+     
+   Note=> If we not use [] then our output is same as when we use the [] but our 
+          data type are changed.
+          when we write  mySymb:"mykey1"  then mySymb is treated as string   
+          when we write  [mySymb]:"mykey1"  then mySymb is treated as Symbol   
+
+
+*/ 
---
+++ 02.JavaScript/04.Object.js
@@ -0,0 +1,97 @@
+// const tinderUser = new Object()  // This is Singlaton Object 
+                                                                  // But the output of both are same.
+const tinderUser = {}    // This is not Singlaton Object 
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "hitesh",
+            lastname: "choudhary"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = { obj1, obj2 }
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+//++++++++++++++++++++++++++++++++ Distructuring of Object +++++++++++++++++++++++++++++++++++
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+//This is another method to access the key and value in object
+
+const {courseInstructor: instructor} = course  // const {the key which we want to access: if we want to change key value then write after (:)} 
+
+// console.log(courseInstructor);
+console.log(instructor);    // change the key name by above methog i.e; const {courseInstructor: instructor} 
+
+
+/*
+// Formate of JSON either in object or an array 
+
+
+// Note=> In json we write key and value both are in Double inverted i.e; " " 
+
+// In object
+{
+    "name": "hitesh",
+    "coursename": "js in hindi",
+    "price": "free"
+}
+
+// In Array
+[
+    {},
+    {},
+    {}
+]
+
+*/
