c64d6b080e25d5987575454aab1eeb20c89d5f85
Aichchhik Kumar <144606935+Ak-Yadav3122@users.noreply.github.com>
2023-10-25 22:21:07 +0530

    Update Prototype.js

--- 09.ClassesAndOops/Prototype.js
+++ 09.ClassesAndOops/Prototype.js
@@ -1,6 +1,5 @@
 // let myName = "hitesh     "
 // let mychannel = "chai     "
-
 // console.log(myName.trueLength);
 
 
@@ -9,53 +8,50 @@ let myHeros = ["thor", "spiderman"]
 
 let heroPower = {
     thor: "hammer",
-    spiderman: "sling",
+    spiderman: "Sling",
 
     getSpiderPower: function(){
-        console.log(`Spidy power is ${this.spiderman}`);
+        console.log(`Spidy hero power is ${this.spiderman}`);
     }
 }
 
-Object.prototype.hitesh = function(){
-    console.log(`hitesh is present in all objects`);
+Object.prototype.golu = function(){
+    console.log(`golu is present in all objects`);
 }
 
-Array.prototype.heyHitesh = function(){
-    console.log(`Hitesh says hello`);
+Array.prototype.heyGolu = function(){
+    console.log(`Aichchhik says hello`);
 }
 
-// heroPower.hitesh()
-// myHeros.hitesh()
-// myHeros.heyHitesh()
-// heroPower.heyHitesh()
+// heroPower.golu()
+// myHeros.golu()
+// myHeros.heyGolu()
+// heroPower.heyGolu()
 
 // inheritance
 
 const User = {
-    name: "chai",
-    email: "chai@google.com"
+    name: "aichchhik",
+    email: "golu@google.com"
 }
-
 const Teacher = {
     makeVideo: true
 }
-
 const TeachingSupport = {
     isAvailable: false
 }
-
 const TASupport = {
-    makeAssignment: 'JS assignment',
+    makeAssignment: 'JS Project',
     fullTime: true,
-    __proto__: TeachingSupport
+    __proto__: TeachingSupport  //  __ this means (--)
 }
 
-Teacher.__proto__ = User
+Teacher.__proto__ = User  //this is old syntax
 
-// modern syntax
+// modern syntax are=> Object.setPrototypeOf(kisme karna hai, kise karna karna hai)
 Object.setPrototypeOf(TeachingSupport, Teacher)
 
-let anotherUsername = "ChaiAurCode     "
+let anotherUsername = "JavaScript and Project     "
 
 String.prototype.trueLength = function(){
     console.log(`${this}`);
@@ -63,5 +59,5 @@ String.prototype.trueLength = function(){
 }
 
 anotherUsername.trueLength()
-"hitesh".trueLength()
-"iceTea".trueLength()
+"aichchhik".trueLength()
+"AichchhhikKumar".trueLength()
